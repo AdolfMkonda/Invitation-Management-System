3c694c4bd56b236b0f63bf2faf186f8b7f62a91d
AdolfMkonda <adolfanatory@gmail.com>
2025-10-12 21:32:02 +0300

    Added invitation API view SET and Configuring the urls for both app and project

---
+++ invitation_management/invitations/urls.py
@@ -0,0 +1,12 @@
+# Importation of necessary modules and classes
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import InvitationViewSet
+from rest_framework.authtoken.views import obtain_auth_token
+
+router = DefaultRouter()
+router.register(r'invitations', InvitationViewSet, basename='invitation')
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+]
--- invitation_management/invitations/views.py
+++ invitation_management/invitations/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Invitation
+from .serializers import InvitationSerializer
+from rest_framework import viewsets, permissions
 
-# Create your views here.
+
+# Invitation View.
+class InvitationViewSet(viewsets.ModelViewSet):
+    queryset = Invitation.objects.all()
+    serializer_class = InvitationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return self.queryset.filter(sender=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(sender=self.request.user)
+
