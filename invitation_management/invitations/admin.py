f91d99f28f24901becf9ac7918661c42e2c9cbcd
AdolfMkonda <adolfanatory@gmail.com>
2025-10-14 08:19:54 +0300

    Creating Administration site for invitation model

--- invitation_management/invitations/admin.py
+++ invitation_management/invitations/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Invitation
+
 
 # Register your models here.
+class InvitationAdmin(admin.ModelAdmin):
+    list_display = ('sender', 'recipient_email', 'status', 'sent_at', 'responded_at')
+    list_filter = ('status', 'sent_at', 'responded_at')
+    search_fields = ('sender__username', 'recipient_email', 'message')
+    ordering = ('-sent_at',)
+
+admin.site.register(Invitation, InvitationAdmin)
+
--- invitation_management/invitations/urls.py
+++ invitation_management/invitations/urls.py
@@ -2,11 +2,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import InvitationViewSet
-from rest_framework.authtoken.views import obtain_auth_token
+from django.contrib import admin
 
 router = DefaultRouter()
 router.register(r'invitations', InvitationViewSet, basename='invitation')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('admin/', admin.site.urls),
 ]
