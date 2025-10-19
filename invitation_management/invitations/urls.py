fbba58562215d48d86892b51366a0f9bc5a376c8
AdolfMkonda <adolfanatory@gmail.com>
2025-10-19 17:09:58 +0300

    Updating  model and setting for proper aurhenticatio,

--- invitation_management/invitations/urls.py
+++ invitation_management/invitations/urls.py
@@ -1,5 +1,6 @@
 # Importation of necessary modules and classes
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.routers import DefaultRouter
 from .views import InvitationViewSet, InvitationListView, InvitationDetailView
 from django.contrib import admin
@@ -10,7 +11,7 @@ router.register(r'invitations', InvitationViewSet, basename='invitation')
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
-    path('invitations/list/', InvitationListView.as_view({'get': 'list'}), name='invitation-list'),
+    path('invitations/list/', InvitationListView.as_view(), name='invitation-list'),
     path('invitations/<int:pk>/', InvitationDetailView.as_view(), name='invitation-detail'),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
--- invitation_management/invitations/views.py
+++ invitation_management/invitations/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from .models import Invitation
 from .serializers import InvitationSerializer
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, generics
+from rest_framework.exceptions import PermissionDenied
 
 
 # Invitation View.
@@ -16,18 +17,16 @@ class InvitationViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(sender=self.request.user)
 
-class InvitationListView(viewsets.ReadOnlyModelViewSet):
-    queryset = Invitation.objects.all()
+class InvitationListView(generics.ListAPIView):
     serializer_class = InvitationSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        return self.queryset.filter(recipient=self.request.user)
+        return Invitation.objects.filter(recipient_email=self.request.user.email)
     
-class InvitationDetailView(viewsets.ReadOnlyModelViewSet):
-    queryset = Invitation.objects.all()
+class InvitationDetailView(generics.RetrieveAPIView):
     serializer_class = InvitationSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        return self.queryset.filter(id=self.kwargs['pk'], recipient=self.request.user)
+        return Invitation.objects.filter(recipient_email=self.request.user.email)
